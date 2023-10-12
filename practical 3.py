c77ff3a3a24b544852594ccff6872477edabb770
adikamble300 <147031016+adikamble300@users.noreply.github.com>
2023-10-12 11:42:04 +0530

    Add files via upload

---
+++ practical 3.py
@@ -0,0 +1,10 @@
+# Program to swap two numbers
+x = input ( "Enter the value of x ")
+y =input ( "Enter the value of y ")
+print  ( "Before swapping")
+print ( "x = ",x)
+print ( "y = ",y)
+y = temp
+print ( "After swapping")
+print ( "x = ",x)
+print ( "y = ",y)
